c97531c6b1b407d8262728928a4c0722c025de7d
Juan Blanco <juanfranblanco@hotmail.com>
2018-03-11 08:57:54 +0000

    2.4.0
    Adding SimpleRpcClient to Rpc repo.

---
+++ src/Nethereum.JsonRpc.RpcClient/SimpleRpcClient.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.Client.RpcMessages;
+using Newtonsoft.Json;
+
+namespace Nethereum.JsonRpc.Client
+{
+    public class SimpleRpcClient : ClientBase
+    {
+        private readonly JsonSerializerSettings _jsonSerializerSettings;
+        private readonly HttpClient _httpClient;
+
+        public SimpleRpcClient(Uri baseUrl, HttpClient httpClient,
+            JsonSerializerSettings jsonSerializerSettings = null)
+        { 
+            if (jsonSerializerSettings == null)
+                jsonSerializerSettings = DefaultJsonSerializerSettingsFactory.BuildDefaultJsonSerializerSettings();
+            _jsonSerializerSettings = jsonSerializerSettings;
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = baseUrl;
+        }
+
+        protected override async Task<T> SendInnerRequestAync<T>(RpcRequest request, string route = null)
+        {
+            var response =
+                await SendAsync(
+                        new RpcRequestMessage(request.Id, request.Method, request.RawParameters), route)
+                    .ConfigureAwait(false);
+            HandleRpcError(response);
+            return response.GetResult<T>();
+        }
+
+        protected override async Task<T> SendInnerRequestAync<T>(string method, string route = null,
+            params object[] paramList)
+        {
+            var request = new RpcRequestMessage(Guid.NewGuid().ToString(), method, paramList);
+            var response = await SendAsync(request, route).ConfigureAwait(false);
+            HandleRpcError(response);
+            return response.GetResult<T>();
+        }
+
+        private void HandleRpcError(RpcResponseMessage response)
+        {
+            if (response.HasError)
+                throw new RpcResponseException(new RpcError(response.Error.Code, response.Error.Message,
+                    response.Error.Data));
+        }
+
+        public override async Task SendRequestAsync(RpcRequest request, string route = null)
+        {
+            var response =
+                await SendAsync(
+                        new RpcRequestMessage(request.Id, request.Method, request.RawParameters), route)
+                    .ConfigureAwait(false);
+            HandleRpcError(response);
+        }
+
+        public override async Task SendRequestAsync(string method, string route = null, params object[] paramList)
+        {
+            var request = new RpcRequestMessage(Guid.NewGuid().ToString(), method, paramList);
+            var response = await SendAsync(request, route).ConfigureAwait(false);
+            HandleRpcError(response);
+        }
+
+        private async Task<RpcResponseMessage> SendAsync(RpcRequestMessage request, string route = null)
+        {
+            try
+            {
+                var rpcRequestJson = JsonConvert.SerializeObject(request, _jsonSerializerSettings);
+                var httpContent = new StringContent(rpcRequestJson, Encoding.UTF8, "application/json");
+
+                var httpResponseMessage = await _httpClient.PostAsync(route, httpContent).ConfigureAwait(false);
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
+                using (var streamReader = new StreamReader(stream))
+                using (var reader = new JsonTextReader(streamReader))
+                {
+                    var serializer = JsonSerializer.Create(_jsonSerializerSettings);
+                    var message = serializer.Deserialize<RpcResponseMessage>(reader);
+
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new RpcClientUnknownException("Error occurred when trying to send rpc requests(s)", ex);
+            }
+        }
+    }
+}
