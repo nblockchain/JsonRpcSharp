c64d9498e91d0bf7c8d92c7c7dc18df1063287e1
Andres G. Aragoneses <knocte@gmail.com>
2019-12-07 13:46:17 +0800

    Modernise nuget.bat

--- nuget.bat
+++ nuget.bat
@@ -1,20 +1,3 @@
 @ECHO OFF
 
-SET COMMUNITY="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET ENTERPRISE="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
-SET FSXSCRIPT1=scripts\cloneFsx.fsx
-SET FSXSCRIPT2=scripts\nuget.fsx
-
-IF EXIST %ENTERPRISE% (
-    SET RUNNER=%ENTERPRISE%
-) ELSE (
-    IF EXIST %COMMUNITY% (
-        SET RUNNER=%COMMUNITY%
-    ) ELSE (
-        ECHO fsi.exe not found, is F# installed?
-        EXIT /b 1
-    )
-)
-
-%RUNNER% %FSXSCRIPT1% %*
-%RUNNER% %FSXSCRIPT2% %*
+CALL scripts\fsi.bat scripts\cloneFsx.fsx %* && CALL scripts\fsi.bat scripts\nuget.fsx %*
